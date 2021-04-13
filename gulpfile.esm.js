e9c68fe57d36ad12e5f7558e4f50eaa72b7ddec2
DanielSrb <srb.dan@gmail.com>
2021-04-13 11:55:39 +0200

    refactor: - do not use chokidar directly - watch task separated - better comments

--- gulpfile.esm.js
+++ gulpfile.esm.js
@@ -14,7 +14,7 @@ const { series, parallel, src, dest, watch } = require('gulp'),
 
 // DEFINE FUNCTIONS
 
-// functions to delete parts of generated files in dist folder
+// 1) functions to delete parts of generated files in dist folder
 
 // delete all files
 const allCleanup = () => del('dist/**/*');
@@ -26,91 +26,89 @@ const htmlCleanup = () => del('dist/**/*.html');
 const cssCleanup = () => del('dist/*.{css,css.map}');
 
 // delete static files
-const staticCleanup = () =>
-    del(
-        [
-            'dist/**/*', // delete all files from /dist/
-            '!dist/**/*.{html,css,css.map}', // except HTML, CSS and CSS map files
-        ],
-        { onlyFiles: true } // do not delete folders (would delete all folders otherwise)
-    );
+const staticCleanup = () => del(
+    [
+        'dist/**/*', // delete all files from /dist/
+        '!dist/**/*.{html,css,css.map}', // except HTML, CSS and CSS map files
+    ],
+    { onlyFiles: true } // do not delete folders (would delete all folders otherwise)
+);
 
-// functions that generate files
+// 2) functions that generate files
 
 // compile twig templates to html files
-const twigCompile = () =>
-    src('src/templates/**/[^_]*.twig')
-        // import data from data.json
-        .pipe(twig({ cache: false, data: JSON.parse(String(readFileSync('src/data.json'))) }))
-        .pipe(dest('./dist/')) // put compiled html into dist folder
-        // tell Browsersync to reload after compiling finishes
-        .on('end', () => browserSync.reload())
+const twigCompile = () => src('src/templates/**/[^_]*.twig')
+    // import data from data.json
+    .pipe(twig({ cache: false, data: JSON.parse(String(readFileSync('src/data.json'))) }))
+    .pipe(dest('./dist/')); // put compiled html into dist folder
 
 // create and process CSS
-const sassCompile = () =>
-    src('src/scss/index.scss') // this is the source of for compilation
-        .pipe(sourcemaps.init()) // initalizes a sourcemap
-        .pipe(sass.sync().on('error', sass.logError)) // compile SCSS to CSS and also tell us about a problem if happens
-        .pipe(
-            postcss([
-                autoprefixer, // automatically adds vendor prefixes if needed
-                // see browserslist in package.json for included browsers
-                // Official Bootstrap browser support policy:
-                // https://getbootstrap.com/docs/4.6/getting-started/browsers-devices/#supported-browsers
-                flexbugsFixes, // fixes flex bugs if possible: see https://github.com/philipwalton/flexbugs
-            ])
-        )
-        .pipe(csso()) // compresses CSS
-        .pipe(sourcemaps.write('./')) // writes the sourcemap
-        .pipe(dest('./dist')) // destination of the resulting CSS
-        .pipe(browserSync.stream()); // tell browsersync to inject compiled CSS
+const sassCompile = () => src('src/scss/index.scss') // this is the source of for compilation
+    .pipe(sourcemaps.init()) // initalizes a sourcemap
+    .pipe(sass.sync().on('error', sass.logError)) // compile SCSS to CSS and also tell us about a problem if happens
+    .pipe(
+        postcss([
+            autoprefixer, // automatically adds vendor prefixes if needed
+            // see browserslist in package.json for included browsers
+            // Official Bootstrap browser support policy:
+            // https://getbootstrap.com/docs/4.6/getting-started/browsers-devices/#supported-browsers
+            flexbugsFixes, // fixes flex bugs if possible: see https://github.com/philipwalton/flexbugs
+        ])
+    )
+    .pipe(csso()) // compresses CSS
+    .pipe(sourcemaps.write('./')) // writes the sourcemap
+    .pipe(dest('./dist')) // destination of the resulting CSS
+    .pipe(browserSync.stream()); // tell browsersync to inject compiled CSS
 
 // remove unused CSS (classes not used in generated HTML)
-const removeUnusedCss = () =>
-    src('dist/index.css')
-        .pipe(
-            postcss([
-                uncss({
-                    html: ['dist/**/*.html'],
-                    media: ['print'], // process additional media queries
-                    ignore: [], // provide a list of selectors that should not be removed by UnCSS
-                }),
-            ])
-        )
-        .pipe(dest('dist'));
+const removeUnusedCss = () => src('dist/index.css')
+    .pipe(
+        postcss([
+            uncss({
+                html: ['dist/**/*.html'],
+                media: ['print'], // process additional media queries
+                ignore: [], // provide a list of selectors that should not be removed by UnCSS
+            }),
+        ])
+    )
+    .pipe(dest('dist'));
 
 // copy all files from /src/static/ to /dist/static/
-const copyStatic = () =>
-    src('src/static/**/*')
-        .pipe(dest('dist'))
-        // tell Browsersync to reload after copying finishes
-        .on('end', () => browserSync.reload())
+const copyStatic = () => src('src/static/**/*').pipe(dest('dist'))
 
+// 3) functions to watch and serve
 // development with automatic refreshing after changes to CSS, templates or static files
-const startBrowsersync = () => {
-    // initalize Browsersync
-    browserSync.init({
-        // port: 8080, // set different port
-        // open: false, // don’t open browser
-        // ghostMode: false, // CLICKS, scrolls & form inputs on any device will not be mirrored to all others
-        // reloadOnRestart: true, // reload each browser when Browsersync is restarted
-        server: {
-            baseDir: 'dist', // serve from this folder
-            serveStaticOptions: {
-                // trying an extension when one isn't specified:
-                // effectively means that http://localhost:3000/another-page
-                // shows file named another-page.html
-                extensions: ['html'],
-            },
+const startBrowsersync = () => browserSync.init({ // initalize Browsersync
+    // port: 8080, // set different port
+    // open: false, // don’t open browser
+    // ghostMode: false, // CLICKS, scrolls & form inputs on any device will not be mirrored to all others
+    // reloadOnRestart: true, // reload each browser when Browsersync is restarted
+    server: {
+        baseDir: 'dist', // serve from this folder
+        serveStaticOptions: {
+            // trying an extension when one isn't specified:
+            // effectively means that http://localhost:3000/another-page
+            // shows file named another-page.html
+            extensions: ['html'],
         },
-    });
-    // watch for changes in SCSS and run task to compile it again
-    watch('src/scss/**/*', series(processCss));
-    // watch for changes in templates and data file and run task to generate HTML again
-    watch(['src/templates/**/*', 'src/data.json'], series(processHtml));
-    // watch for changes in static files and run task to copy them again
-    watch('src/static/**/*', series(processStatic));
+    },
+});
+
+// a function to reload Browsersync
+const reloadBrowserSync = (cb) => {
+    browserSync.reload();
+    cb();
 };
+
+// a function to watch for changes
+const watchFiles = () => {
+    // SCSS changed: run task to compile it again
+    watch('src/scss/**/*', processCss);
+    // templates or data file changed: run task to generate HTML again and reload
+    watch(['src/templates/**/*', 'src/data.json'], series(processHtml, reloadBrowserSync));
+    // static files changed: run task to copy them again and reload
+    watch('src/static/**/*', series(processStatic, reloadBrowserSync));
+}
 
 // COMPOSE TASKS
 
@@ -120,15 +118,24 @@ const processCss = series(cssCleanup, sassCompile);
 
 const processStatic = series(staticCleanup, copyStatic);
 
-// PUBLICLY AVAILABLE TASKS
+// EXPORT PUBLICLY AVAILABLE TASKS
 // These tasks can be run with `npx gulp TASKNAME` on command line for example `npx gulp develop`.
 // We use them in npm scripts with `gulp TASKNAME` (see package.json).
 
 // development with automatic refreshing
-exports.develop = series(allCleanup, parallel(twigCompile, sassCompile, copyStatic), startBrowsersync);
+exports.develop = series(
+    allCleanup,
+    parallel(twigCompile, sassCompile, copyStatic),
+    parallel(startBrowsersync, watchFiles)
+);
 
 // build everything for production
-exports.build = series(allCleanup, twigCompile, parallel(sassCompile, copyStatic), removeUnusedCss);
+exports.build = series(
+    allCleanup,
+    twigCompile,
+    parallel(sassCompile, copyStatic),
+    removeUnusedCss
+);
 
 // the default task runs when you run just `gulp`
 exports.default = exports.develop;
